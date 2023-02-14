2a1a9ce4de44a366e21552e5c3b65dfd498921ea
llima <llima@nelogica.com.br>
2023-02-14 17:27:06 -0300

    Add more items in carousel

--- src/views/GiftCard/index.jsx
+++ src/views/GiftCard/index.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Carousel from "../../components/shared/Carousel";
-import Container from "./styles";
+import Container, { CarouselTitle } from "./styles";
 import { carouselData } from "../../data/carousel";
 
 
 const GiftCard = () => {
+  const carousels = carouselData.map((el) => {
+    return (
+      <>
+        <CarouselTitle>{el.displayName}</CarouselTitle>
+        <Carousel carouselData={el.data}/>
+      </>
+    )
+  })
   return (
-    <Container>
-      <Carousel carouselData={carouselData}/>
-      <Carousel carouselData={carouselData}/>
-    </Container>
+    <Container>{carousels}</Container>
   )
 }
 export default GiftCard;
--- src/views/GiftCard/styles.js
+++ src/views/GiftCard/styles.js
@@ -4,4 +4,8 @@ export const Container = styled.div`
   width: 100%;
 `;
 
+export const CarouselTitle = styled.div`
+  
+`;
+
 export default Container;
